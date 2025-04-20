f7acda1ac59e93bf56a796e0e13f799d4a309844
SuperRama <superrama86@gmail.com>
2025-04-20 15:28:59 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+import React from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <p className="read-the-docs">
+        Hello word
+      </p>
+    </>
+  )
+}
+
+export default App
